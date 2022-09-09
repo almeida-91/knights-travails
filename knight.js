540c0e544110fbbd80cf801b2638ff19fd3fd3c6
Almeida-91 <aplalmeida91@outlook.com>
2022-09-09 23:08:36 +0100

    Add function to generate knights to get to the goal in a BFS algorithm

--- knight.js
+++ knight.js
@@ -10,11 +10,18 @@ class board {
     let currentMove;
     for (let i = 0; i < 8; i++) {
       for (let j = 0; j < 8; j++) {
-        position = [i, j];
+        position = [j, i];
         currentMove = new move([i, j]);
         this.array.push(currentMove);
       }
     }
+  }
+
+  isFull(){
+    let checkfull = this.array.filter(obj => { 
+        return obj.isVisited == false;
+    })
+    return checkfull.length==0 ? true : false;
   }
 }
 
@@ -29,9 +36,9 @@ class knight {
   constructor(start, board) {
     this.root = start;
     this.possibleMoves = [];
-    this.nextMove = null;
+    this.nextMoves = [];
     this.getPossibleMoves(board);
-    board.array[(start[0], start[1])].isVisited = true;
+    board.array[(start[0]*8+start[1])].isVisited = true;
   }
 
   newMove(array) {
@@ -40,7 +47,7 @@ class knight {
     return array;
   }
 
-  getPossibleMoves(board) {
+  getPossibleMoves() {
     let start = this.root;
     this.possibleMoves.push(this.newMove([start[0] + 1, start[1] + 2]));
     this.possibleMoves.push(this.newMove([start[0] + 1, start[1] - 2]));
@@ -50,60 +57,63 @@ class knight {
     this.possibleMoves.push(this.newMove([start[0] + 2, start[1] - 1]));
     this.possibleMoves.push(this.newMove([start[0] - 2, start[1] + 1]));
     this.possibleMoves.push(this.newMove([start[0] - 2, start[1] - 1]));
-    this.filterIlegalMoves(board);
+    this.filterIlegalMoves();
   }
 
   checkGoal(goal) {
-    let moveList = this.possibleMoves;
-    let goalRoot = goal.root;
-    let goalReached = false;
-    let index = 0;
-    while (goalReached == false && index < moveList.length) {
-      goalReached = moveList[index].every((r) => goalRoot.includes(r));
-      index++;
-    }
-    if (goalReached == false) return false;
-    return goalReached;
+    let knightX = this.root[0];
+    let knightY = this.root[1];
+    let goalX = goal[0];
+    let goalY = goal[1];
+    return (knightX == goalX && knightY == goalY);
   }
 
-  getNewKnights(board) {
-    let moveList = this.possibleMoves;
-    for (let i = 0; i < moveList.length; i++) {
-      moveList[i] = new knight(moveList[i], board);
+  getNewKnights(board,goal) {
+    if (board.isFull()==true) return null;
+    let previousKnight = new knight(this.root,board);
+    let queue = previousKnight.possibleMoves;
+    let currentKnight;    
+    while (board.isFull()==false){
+        let currentMoves = queue.shift();
+        let boardX = currentMoves[0];
+        let boardY = currentMoves[1];
+        if (board.array[(boardX*8+boardY)].isVisited == false){
+            currentKnight = new knight(currentMoves,board);
+            currentKnight.nextMoves.push(previousKnight);
+            if (currentKnight.checkGoal(goal)== true) return currentKnight;
+            previousKnight = currentKnight;
+            currentKnight.possibleMoves.forEach(item => queue.push(item));
+        }
     }
+    return currentKnight;
   }
 
-  filterIlegalMoves(board) {
+  filterIlegalMoves() {
     this.possibleMoves = this.possibleMoves.filter((item) => {
       return item != undefined;
     });
-  }
-
-  findWay(goal) {
-    let goalRoot = goal;
-    let start = this.root;
   }
 }
 
 function knightMoves(start, goal, gameBoard) {
   if (start[0] > 8 || start[1] > 8) return "ilegal";
+  if (start[0] == goal[0] && start[1] == goal[1]) return "Knight is already in goal";
   let knightStart = new knight(start, gameBoard);
-  /* if (knightStart.checkGoal(knightGoal) == false){
-        let nextMoves = knightStart.possibleMoves;
-        console.log(nextMoves);
-        for (let i = 0 ; i < nextMoves.length ; i++){
-            let move = new knight(nextMoves[i]);
-            console.log(move);
-            if (move.checkGoal(knightGoal) == true) return 'success';
-        } */
-
-  //}
-  /* console.log(knightStart.checkGoal(knightGoal));
-    return `[${knightStart.root}] [${knightGoal.root}]`; */
+  let goalX = goal[0];
+  let goalY = goal[1];
+  //knightStart.getNewKnights(gameBoard);
+/*   while (gameBoard.array[ goalX , goalY ].isVisited == false){
+    knightStart = knightStart.possibleMoves;
+    for (let i = 0 ; i < knightStart.length ; i++){
+        knightStart[i].getNewKnights(gameBoard);
+    }
+  } */
+  console.log('END');
+  return `[${knightStart.root}] [${goal}]`;
 }
 
 let start = [0, 0];
-let goal = [1, 2];
+let goal = [3, 3];
 let b = new board();
 
 knightMoves(start, goal, b);
